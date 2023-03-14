8f4ff542ff15cf5518f8aad542450bd84a3c9fcb
Andrea Fianco <andrea.fianco@gmail.com>
2023-03-14 16:56:38 +0100

    Add 404 page and routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
